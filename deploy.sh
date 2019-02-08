7ea5bb9ed9be49dc57a380fc5072d150268a020d
Sergey <sergeyshaykhullin@gmail.com>
2019-02-08 03:16:07 +0300

    1.0.0 travis

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 PREVIOUS_VERSION=`expr $TRAVIS_BUILD_NUMBER - 1`
 
-echo docker pull otakushelter/otaku-shelter.auth:1.0.$TRAVIS_BUILD_NUMBER
-echo docker stop otakushelter/otaku-shelter.auth:1.0.$PREVIOUS_VERSION
-echo docker rmi otakushelter/otaku-shelter.auth:1.0.$PREVIOUS_VERSION -f 
-echo docker run -d otakushelter/otaku-shelter.auth:1.0.$TRAVIS_BUILD_NUMBER
+echo docker pull otakushelter/otaku-shelter.account:1.0.$TRAVIS_BUILD_NUMBER
+echo docker stop otakushelter/otaku-shelter.account:1.0.$PREVIOUS_VERSION
+echo docker rmi otakushelter/otaku-shelter.account:1.0.$PREVIOUS_VERSION -f 
+echo docker run -d otakushelter/otaku-shelter.account:1.0.$TRAVIS_BUILD_NUMBER
